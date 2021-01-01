9c90be6e1411109c79a79906c13c2b68d9c3a245
Norlyn Piquero <52349385+norlynpiquero@users.noreply.github.com>
2021-01-01 13:23:36 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const navbarOffsetTop = navbar.offsetTop;
 const sections = document.querySelectorAll("section");
 const navbarLinks = document.querySelectorAll(".navbar-link");
 const progress = document.querySelector(".progress-bars-wrapper");
-const progressBarPercents = [98, 90, 60, 50, 90];
+const progressBarPercents = [98, 90, 90];
 
 window.addEventListener("scroll", () => {
   mainFn();
